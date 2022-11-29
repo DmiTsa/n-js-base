74adb0fd81459d8872719f6264ed9fc16db6c261
DmiTsa <you@example.com>
2022-11-29 21:08:56 +0300

    createjs - ok

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,19 @@
 import { appendFile, access, constants } from 'node:fs/promises';
-import { join } from 'node:path';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const create = async () => {
-  const path = './src/fs/files';
+  const pathRun = dirname(fileURLToPath(import.meta.url));
+  const pathDest = '/files';
   const fileName = 'fresh.txt';
   const data = 'I am fresh and young';
+  const errorMsg = 'FS operation failed';
 
   try {
-    await access(join(path, fileName), constants.F_OK);
-    console.error('Сбой операции файловой системы');
+    await access(join(pathRun, pathDest, fileName), constants.F_OK);
+    console.error(errorMsg);
   } catch {
-    appendFile(join(path, fileName), data);
+    appendFile(join(pathRun, pathDest, fileName), data);
   }
 };
 
