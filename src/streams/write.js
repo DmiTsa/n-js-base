c6773a7b8e6adfec6b1cb92a56374db4e04b7b8b
DmiTsa <you@example.com>
2022-11-30 21:16:46 +0300

    4

--- src/streams/write.js
+++ src/streams/write.js
@@ -5,14 +5,9 @@ const write = async () => {
   const path = getPath('files', 'fileToWrite.txt');
   const writeStream = createWriteStream(path);
 
-  console.log('input any data');
-
-  process.stdin.on('data', (chank) => {
-    writeStream.write(chank);
+  process.stdin.on('data', (ch) => {
+    writeStream.write(ch);
   });
 };
 
 await write();
-
-// write.js — реализовать функцию, которая записывает данные
-// process.stdin в файл fileToWrite.txt с использованием Writable Stream.
