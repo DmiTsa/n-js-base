7758b02f06aa1ca8d443b096699fd846d426fa33
DmiTsa <you@example.com>
2022-11-30 20:15:15 +0300

    1

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,31 +1,15 @@
-// import { createReadStream } from 'node:fs';
-import { Readable } from 'node:stream';
-import { stdin, stdout } from 'node:process';
+import { createReadStream } from 'node:fs';
+import getPath from './getPath.js';
 
 const read = async () => {
-  //   const readStream = createReadStream('c:/test.txt');
-  //   readStream.on('readable', () => {
-  //     console.log(readStream.read());
-  //   });
-
-  for await (const chunk of Readable.from('c:/test.txt')) {
-    console.log(chunk);
-  }
-  const r = stdout.read('re');
-  console.log(r);
+  const path = getPath('files', 'fileToRead.txt');
+  const readStream = createReadStream(path);
+  readStream.on('data', (chank) => {
+    process.stdout.write(chank);
+  });
 };
 
 await read();
-// readable.setEncoding(encoding)
 
 // read.js — реализовать функцию, которая читает содержимое файла fileToRead.txt
 // с помощью Readable Stream и выводит его содержимое в process.stdout.
-
-//// const fs = require('node:fs');
-// //const rr = fs.createReadStream('foo.txt');
-// rr.on('readable', () => {
-// console.log(`readable: ${rr.read()}`);
-// });
-// rr.on('end', () => {
-//   console.log('end');
-// });
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from 'node:fs';
+import getPath from './getPath.js';
+
 const write = async () => {
-    // Write your code here 
+  const path = getPath('files', 'fileToWrite.txt');
+  const writeStream = createWriteStream(path);
+
+  console.log('input any data');
+
+  process.stdin.on('data', (chank) => {
+    writeStream.write(chank);
+  });
 };
 
 await write();
+
+// write.js — реализовать функцию, которая записывает данные
+// process.stdin в файл fileToWrite.txt с использованием Writable Stream.
