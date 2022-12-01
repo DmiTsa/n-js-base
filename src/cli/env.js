c7de51b528264b3b6c92a601c07fdf753a8d68fd
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2022-12-01 15:27:26 +0300

    10

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,24 @@
 // env.js - реализовать функцию, которая анализирует переменные окружения с префиксом RSS_ и выводит их на консоль в формате RSS_name1=value1; RSS_имя2=значение2
 const parseEnv = () => {
-  // Write your code here
+  const envVar = process.env;
+  console.log(envVar); //{ key: value, key: value}
+
+  for (const key in envVar) {
+    if (Object.hasOwnProperty.call(envVar, key)) {
+      const element = envVar[key];
+      console.log('aaaaa' + element);
+    }
+  }
+  // const rssVar = envVar.reduce((ac, varObj) => {
+  //   if (varObj.key === 'VSCODE_PID') {
+  //     ac.push(varObj);
+  //     return ac;
+  //   }
+  // }, []);
+  // console.log(rssVar);
 };
 
 parseEnv();
+
+// "cli-env": "npx cross-env SOME=any RSS_foo=bar RSS_bar=baz node src/cli/env.js",
+//VSCODE_PID
