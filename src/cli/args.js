68938b99dbb8994e66cda206d17731b4b2bd4c9d
DmiTsa <you@example.com>
2022-12-01 19:19:28 +0300

    cli is ok

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,24 +1,14 @@
 const parseArgs = () => {
-  const ar = process.argv;
+  const args = process.argv;
+  const props = [];
 
-  console.log('-----------------------');
+  args.map((el, i) => {
+    if (el.slice(0, 2) === '--') {
+      props.push(`${el} is ${args[i + 1]}`);
+    }
+  });
 
-  console.log(ar);
-
-  // Write your code here
+  console.log(props.join(' ,'));
 };
 
 parseArgs();
-
-// args.js — реализовать функцию, которая анализирует аргументы командной строки (данные в формате --propName value --prop2Name value2, вам не нужно его проверять) и выводит их на консоль в формате propName is value, prop2Name is value2
-
-// [
-//     'C:\\Program Files\\nodejs\\node.exe',
-//     'C:\\js-app\\n-js-base\\src\\cli\\args.js',
-//     '--some-arg',
-//     'value1',
-//     '--other',
-//     '1337',
-//     '--arg2',
-//     '42'
-//   ]
