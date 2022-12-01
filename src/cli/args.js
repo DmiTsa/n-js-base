763f761c1033b0cdbe1a7b56c3891770ed35aa6a
Dmitry Taskaev <dmitrytaskaev.dt@gmail.com>
2022-12-01 16:52:01 +0300

    12

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
 const parseArgs = () => {
-    // Write your code here 
+  const ar = process.argv;
+
+  console.log('-----------------------');
+
+  console.log(ar);
+
+  // Write your code here
 };
 
 parseArgs();
+
+// args.js — реализовать функцию, которая анализирует аргументы командной строки (данные в формате --propName value --prop2Name value2, вам не нужно его проверять) и выводит их на консоль в формате propName is value, prop2Name is value2
+
+// [
+//     'C:\\Program Files\\nodejs\\node.exe',
+//     'C:\\js-app\\n-js-base\\src\\cli\\args.js',
+//     '--some-arg',
+//     'value1',
+//     '--other',
+//     '1337',
+//     '--arg2',
+//     '42'
+//   ]
