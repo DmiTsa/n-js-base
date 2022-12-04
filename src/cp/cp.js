c09a98cb5a8c9d780f31671cfc8ff72cef1bcc02
DmiTsa <you@example.com>
2022-12-04 15:59:26 +0300

    done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { spawn } from 'node:child_process';
+import getPath from '../fs/getPath.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathCPFile = getPath('/files', 'script.js');
+  const spawnProc = spawn('node', [pathCPFile]);
+
+  process.stdin.on('data', () => {
+    spawnProc.stdout.pipe(process.stdin);
+    spawnProc.stdin.pipe(process.stdout);
+  });
 };
 
 spawnChildProcess();
